dea081c2348de7d8d8272d3bd2ccc53c1a9126f4
Parth Pandey <101416127+DarkRai087@users.noreply.github.com>
2023-12-08 09:43:32 +0530

    2620. Counter

---
+++ Counter.js
@@ -0,0 +1,8 @@
+// Define a function named createCounter that takes a 
+//parameter n.
+var createCounter = function(n) {
+    
+    // Return an arrow function.
+    return () => n++;
+        
+};
